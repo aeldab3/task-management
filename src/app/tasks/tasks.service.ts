8ecc552f9e680ebc704548273fa2138ec8a5780a
Ahmed Eldab3 <aeldabaa2@gmail.com>
2025-08-17 17:45:46 +0300

    feat(tasks): add localstorage

--- src/app/tasks/tasks.service.ts
+++ src/app/tasks/tasks.service.ts
@@ -4,23 +4,35 @@ import { DUMMY_TASKS } from "../dummy-tasks";
 
 @Injectable({ providedIn: 'root' })
 export class TasksService {
-    private tasks = DUMMY_TASKS;
-    
-      getUserTasks(userId: string) {
-        return this.tasks.filter(task => task.userId === userId)
-      }
-    
-      deleteTask(taskId: string) {
-        this.tasks = this.tasks.filter(task => task.id !== taskId);
-      }
-    
-      addTask(taskData : InewTaskData, userId: string) {
-        this.tasks.unshift({
-          id: new Date().getTime().toString(),
-          userId: userId,
-          title: taskData.title,
-          summary: taskData.summary,
-          dueDate: taskData.date
-        });
-      }
+  private tasks = DUMMY_TASKS;
+  constructor(){
+    const tasks = localStorage.getItem('tasks');
+    if (tasks) {
+      this.tasks = JSON.parse(tasks);
+    }
+  }
+  
+  getUserTasks(userId: string) {
+    return this.tasks.filter(task => task.userId === userId)
+  }
+
+  deleteTask(taskId: string) {
+    this.tasks = this.tasks.filter(task => task.id !== taskId);
+    this.saveTasks();
+  }
+
+  addTask(taskData : InewTaskData, userId: string) {
+    this.tasks.unshift({
+      id: new Date().getTime().toString(),
+      userId: userId,
+      title: taskData.title,
+      summary: taskData.summary,
+      dueDate: taskData.date
+    });
+    this.saveTasks();
+  }
+
+  private saveTasks() {
+    localStorage.setItem('tasks', JSON.stringify(this.tasks));
+  }
 }
